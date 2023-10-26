a65056b1cb893cf16ac0df544aa3e516de0d46a0
AlexandraYartseva-QA <149089347+AlexandraYartseva-QA@users.noreply.github.com>
2023-10-26 18:59:50 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,12 @@
+/* Это объявление переменной, мы наши кнопку по тегу */
+const button = document.querySelector('button');
+
+/* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
+button.addEventListener('click', function() {
+	alert('Цели сами себя не достигнут) Так что действуй, все в твоих руках:)')
+})
+
+
+button.addEventListener('click', function openImage(gif){
+	image='gif1.gif'
+})
